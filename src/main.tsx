ceb509e323bf34e32ffcaa69044b66fa874f54cc
sirojUz <sirojiddinmamatovuzb@gmail.com>
2024-10-05 06:49:51 +0900

    added husky hook

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(
+  document.getElementById("root") ?? document.createElement("div")
+).render(<App />);
+
+// dasturchilarning xabarlari "conventional-commits" orqali nazorat qilinadi;
+// types other than fix: and feat: are allowed, for example @commitlint/config-conventional
+// recommends build:, chore:, ci:, docs:, style:, refactor:, perf:, test:, and others.
